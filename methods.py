49b87a4fef20810215507882f7adc56d8d901e4a
Bartek <bartek@Macbooks-MBP.lan>
2017-12-17 14:15:20 +0100

    changes

--- methods.py
+++ methods.py
@@ -3,7 +3,7 @@ import re
 
 def getImage(artist):
     modifiedString = artist.replace("_", "%20")
-    url = "https://www.bing.com/images/search?q=" + modifiedString + "&qs=n&form=QBIR&sp=-1&pq=" + modifiedString.lower()
+    url = "https://www.bing.com/images/search?q=" + modifiedString + "&qs=n&form=QBIR&sp=-1&pq=" + modifiedString.lower() + "&sc=8-11"
     br = RoboBrowser()
     br.open(url)
 
