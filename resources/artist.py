3cf6a9fd9dfa6dce0bc5be216fab9db60ca7e9b3
Bartek <bartek@Macbooks-MBP.lan>
2017-12-22 12:38:45 +0100

    Changes

--- resources/artist.py
+++ resources/artist.py
@@ -1,12 +1,11 @@
 from flask_restful import Resource
 from models.artistModel import ArtistModel
 import methods
-from app import totalRequestCount
+
 class Artist(Resource):
 
     def get(self, artist):
         result = ""
-        totalRequestCount += 1
 
         try:
             result = methods.getArtist(artist)
@@ -16,6 +15,5 @@ class Artist(Resource):
         if result == {"Message": "Not found"}:
             return {"message": "Not found"}, 400
 
-        print(result)
         newArtist = ArtistModel(*result)
         return newArtist.json(), 200
