d009abedf999544402955a568102cceea444d7d2
Bartek <bartek@Macbooks-MBP.lan>
2018-01-21 14:24:33 +0100

    Cleanup

--- app.py
+++ app.py
@@ -10,4 +10,4 @@ api.add_resource(Artist, "/artist/<string:artist>")
 api.add_resource(Lyrics, "/lyrics")
 
 if __name__ == "__main__":
-    app.run(port=5000, debug=True)
+    app.run(port=5000, debug=False)
--- methods.py
+++ methods.py
@@ -7,9 +7,9 @@ def makeUrl(artist):
     modifiedArtistString = modifiedArtistString[0].upper() + modifiedArtistString[1::]
     return "https://genius.com/artists/" + modifiedArtistString
 
-def getSoupHTML(query):
+def getSoupHTML(url):
     header = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36"}
-    req = Request(query, headers=header)
+    req = Request(url, headers=header)
 
     try:
         html = urlopen(req)
@@ -32,41 +32,6 @@ def descFromWiki(artist):
     soup = Soup(html, "html.parser")
 
     extract = [x for x in str(soup.find("p").text)]
-
-    count = 0
-    count1 = 0
-    periodCount = 0
-    stopIndex = len(extract)-1
-    frenzyMode = False
-    while count != len(extract)-1:
-        if periodCount == 2:
-            stopIndex = count
-            break
-
-        char = extract[count]
-
-        if char == ".":
-            periodCount += 1
-
-        if char == "/":
-            count1 += 1
-            frenzyMode = True
-
-        if char in "[":
-            frenzyMode = True
-
-        if frenzyMode:
-            extract.pop(count)
-        else:
-            count += 1
-
-        if char in "]":
-            frenzyMode = False
-
-        if char == "/":
-            if count1 == 2:
-                count1 = 0
-                frenzyMode = False
 
     return "".join(extract[0:stopIndex])
 
@@ -91,7 +56,6 @@ def artistNameFromGenius(page):
         if char == "\n":
             break
         cleanName += char
-    print(cleanName)
 
     return cleanName
 
@@ -121,121 +85,3 @@ def getLyricsFromGenius(url):
     result = [x.split("\n") for x in result]
 
     return result
-
-# def getArtist(artist):
-#     modifiedArtistString = artist.replace("_", "-").lower()
-#     modifiedArtistString = modifiedArtistString[0].upper() + modifiedArtistString[1::]
-#
-#     header = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36"}
-#     url = "https://genius.com/artists/" + modifiedArtistString
-#     print(url)
-#     req = Request(url, headers=header)
-#     html = urlopen(req)
-#     soup = Soup(html, "html.parser")
-#
-#     divWithImage = soup.find("div", "user_avatar")
-#     imageURL = re.search(r'http([^\'" >]+)jpg', str(divWithImage)).group(0)
-#
-#     descriptionParagraphs = [x.text for x in soup.findAll("p")]
-#
-#     topSongsGrid = soup.findAll("div", "mini_card_grid-song")[0:5]
-#     topSongs = []
-#
-#     for i in range(len(topSongsGrid)):
-#         section = topSongsGrid[i]
-#
-#         topSongs.append([section.find("div", "mini_card-title").text.strip()])
-#         topSongs[i].append(section.find("div", "mini_card-subtitle").text.strip())
-#         topSongs[i].append(re.search(r'http([^\'" >]+)', str(section.find("div", "mini_card-thumbnail"))).group(0))
-#         topSongs[i].append(re.search(r'http([^\'" >]+)lyrics', str(section.find("a", "mini_card"))).group(0))
-#
-#     for p in descriptionParagraphs:
-#         print("\n")
-#         print(p)
-#
-#     print(imageURL)
-#
-#     for section in topSongs:
-#         print("\n")
-#         for s in section:
-#             print(s)
-#
-#     return [descriptionParagraphs, imageURL, topSongs]
-#
-# def getImage(artist):
-#     modifiedString = artist.replace("_", "%20")
-#     url = "https://www.bing.com/images/search?q=" + modifiedString + "&qs=n&form=QBIR&sp=-1&pq=" + modifiedString.lower() + "&sc=8-11"
-#     br = RoboBrowser()
-#     br.open(url)
-#
-#     html = str(br.parsed)
-#
-#     result = re.findall(r'http([^\'" >]+)jpg', html)
-#     myList = []
-#
-#
-#     for i in range(len(result)):
-#         if i == 10:
-#             break
-#         myList.append("http" + result[i] + "jpg")
-#
-#     return myList
-#
-# def getDesc(artist):
-#     htmlBody = urllib.request.urlopen("https://en.wikipedia.org/wiki/ " + artist)
-#     soup = Soup(htmlBody, "html.parser")
-#
-#     extract = [x for x in str(soup.find("p").text)]
-#
-#     count = 0
-#     count1 = 0
-#     periodCount = 0
-#     stopIndex = len(extract)-1
-#     frenzyMode = False
-#     while count != len(extract)-1:
-#         if periodCount == 2:
-#             stopIndex = count
-#             break
-#
-#         char = extract[count]
-#
-#         if char == ".":
-#             periodCount += 1
-#
-#         if char == "/":
-#             count1 += 1
-#             frenzyMode = True
-#
-#
-#         if char in "[":
-#             frenzyMode = True
-#
-#         if frenzyMode:
-#             extract.pop(count)
-#         else:
-#             count += 1
-#
-#         if char in "]":
-#             frenzyMode = False
-#
-#         if char == "/":
-#             if count1 == 2:
-#                 count1 = 0
-#                 frenzyMode = False
-#
-#     return ["".join(extract[0:stopIndex]), soup.find("span", "bday").text]
-#
-# def getTopSongs(artist):
-#     modifiedArtistString = artist.replace("_", "-").lower()
-#     modifiedArtistString = modifiedArtistString[0].upper() + modifiedArtistString[1::]
-#
-#     hdr = {'User-Agent':'Mozilla/5.0'}
-#     url = "https://genius.com/artists/" + modifiedArtistString
-#
-#     req = urllib.request.Request(url, headers=hdr)
-#     html = urllib.request.urlopen(req)
-#     soup = Soup(html, "html.parser")
-#
-#     extract = [x.text for x in soup.findAll("div", "mini_card-title")[0:5]]
-#
-#     return extract
